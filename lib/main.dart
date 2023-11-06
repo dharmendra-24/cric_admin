fcc828ef6b451a130a0d121b8718e01d841c7875
Dharmendra Baghel <dharmendrabaghel2900@gmail.com>
2023-11-06 17:59:24 +0530

    title

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Cricket website',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -37,5 +37,5 @@ class MyApp extends StatelessWidget {
       ),
       home: SignUpScreen(),
     );
-}
+  }
 }
